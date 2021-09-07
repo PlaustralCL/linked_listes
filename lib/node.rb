2d719f31068d549cb1aa517294c831919546af1b
PlaustralCL <plaustralcl@gmail.com>
2021-09-06 20:40:59 -0400

    Create Node class

---
+++ lib/node.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Create individual nodes for the linked list
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
